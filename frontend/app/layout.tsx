cdc0df36abf51705151eda4d641dfa4812664cb8
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-06-21 05:49:36 +0100

    Reconfiguring the frontend

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../providers/Providers";
-import { Navbar } from "../frontend/components/shared/Navbar";
+import { Navbar } from "../components/shared/Navbar";
 import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 
