e1659c198c260ac6a59f173d717820d48a4dc336
Sharon Emmanuel <sharonemma2000@gmail.com>
2026-06-22 23:01:16 +0100

    building the smart contract

--- contracts/contracts/FundFlow.sol
+++ contracts/contracts/FundFlow.sol
@@ -16,3 +16,49 @@ interface IBackBadge {
 ///         contributor voting. Implements Checks-Effects-Interactions throughout.
 /// @dev    Inherits ReentrancyGuard, Ownable, Pausable from OpenZeppelin v5.
 
+// Errors
+
+// Constants
+uint256 public constant MAX-MILESTONES = 10;
+uint256 public constant VOTING_PERIOD = 3 days;
+uint256 public constant MIN_DURATION = 1 days;
+uint256 public constant MAX-DURATION = 90 days;
+
+// Enums
+enum CampaignStatus { Active, Successful, Failed, Completed }
+enum VoteOption     { Approve, Reject }
+
+// Structs
+struct Milestone {
+    string title;
+    uint256 amount;
+    bool completed;
+    bool approved;
+    bool requested;
+    uint256 votingDeadline;
+    uint256 approvedVotes;
+    uint256 rejectedVotes;
+}
+
+ struct Campaign {
+        address        creator;
+        string         title;
+        string         description;
+        uint256        goal;
+        uint256        raisedAmount;
+        uint256        deadline;
+        CampaignStatus status;
+        uint256        contributorCount;
+        uint256        currentMilestoneIndex;
+        bool           exists;
+    }
+
+// States
+uint256 public campaignCount;
+uint256 public totalPlatFormfees;
+address public backerBadgeContract;
+
+mapping(uint256 => Campaign) public campaigns;
+mapping(uint256 => MileStone[]) public campaignMilestones;
+mapping(uint256 => mapping(address => uint256)) public campaignContributions;
+mapping(uint256 => mapping(address => bool)) public hasRefunded;
